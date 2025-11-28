646917127e06bef816b99e073b7517ac067d790f
Matheus <matheuszanettea@gmail.com>
2025-11-28 13:43:09 -0300

    first commit

---
+++ build/CMakeFiles/SimColisao.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimColisao.dir/main.c.o"
+  "CMakeFiles/SimColisao.dir/main.c.o.d"
+  "SimColisao"
+  "SimColisao.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SimColisao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
